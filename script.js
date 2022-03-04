44dcf5e194a785ee6ad65914431a307536844814
Maria Luiza M <maria.lmm.975@gmail.com>
2022-03-04 20:11:25 -0300

    Adiciona tela principal de forma dinamica pelo js

---
+++ script.js
@@ -0,0 +1,27 @@
+function renderizarTelaPrincipal(informacoesClima){
+    const tempo = document.querySelector('.tempo');
+    tempo.innerHTML = `
+        <article class="principal">
+            <h3>${informacoesClima.cidade}</h3>
+            <h2>${informacoesClima.descricao}</h2>
+            <div>
+                <img src="${informacoesClima.imagem}" alt="imagem ${informacoesClima.descricao}">
+                <h1>${informacoesClima.temperatura} °C</h1>
+            </div>
+        </article>
+
+        <article class="temperaturas">
+            <p>Sensação: ${informacoesClima.sensacao} °C</p>
+            <div class="max-min">
+                <p class="max">Max: ${informacoesClima.maxima} °C</p>
+                <p class="min">Min: ${informacoesClima.minima} °C</p>
+            </div>
+        </article>
+
+        <article class="outros">
+            <p class="chuva">Volume de precipitação(última hora): ${informacoesClima.chuva} mm</p>
+            <p class="vento">Vento: ${informacoesClima.vento} m/s</p>
+            <p class="umidade">Umidade: ${informacoesClima.umidade}%</p>
+        </article>
+    `
+}
